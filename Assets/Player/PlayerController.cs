5e98c0c333768412c07d6cb78905aa1376dc53d5
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2018-08-02 14:00:59 +0900

    Create Stage

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody MyBody;
+
+    float InitWalkPower = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+        this.MyBody = GetComponent<Rigidbody>();
+        gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float DownKey = KeyDown();
+
+        float speedx = Mathf.Abs(this.MyBody.velocity.x);
+
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.MyBody.AddForce(transform.right * DownKey * this.InitWalkPower);
+        }
+        if (DownKey != 0)
+        {
+            transform.localScale = new Vector3(DownKey, 0.5f, 0.5f);
+        }
+
+    }
+
+    private float KeyDown()
+    {
+        float result = 0;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            result = 0.5f;
+        }
+        else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            result = -0.5f;
+        }
+        return result;
+    }
+}
